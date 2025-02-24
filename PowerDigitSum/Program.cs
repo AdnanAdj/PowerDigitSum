2f17a7d6451f067a8c2878d559acb23da1d1f901
Adnan Jelani <adnanjelaniadj@gmail.com>
2025-02-24 12:59:13 +0500

    Add project files.

---
+++ PowerDigitSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number greater than 1000: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n <= 1000)
+        {
+            Console.WriteLine("Invalid input! Number must be greater than 1000.");
+            return;
+        }
+
+        int result = CalculatePowerSum(n);
+        Console.WriteLine($"Output: {result}");
+    }
+
+    public static int CalculatePowerSum(int num)
+    {
+        // Implement your logic here
+        return 0; // Replace this with the correct result
+    }
+}
---
+++ PowerDigitSum/Tests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [TestCase(2453, 216)]   // 2^3 + 4^3 + 5^3 + 3^3 = 8 + 64 + 125 + 27 = 216
+        [TestCase(1234, 100)]   // 1^3 + 2^3 + 3^3 + 4^3 = 1 + 8 + 27 + 64 = 100
+        [TestCase(5678, 1421)]  // 5^3 + 6^3 + 7^3 + 8^3 = 125 + 216 + 343 + 512 = 1421
+        [TestCase(9999, 2916)]  // 9^3 + 9^3 + 9^3 + 9^3 = 729 + 729 + 729 + 729 = 2916
+        public void TestCalculatePowerSum(int input, int expected)
+        {
+            int actual = Program.CalculatePowerSum(input);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
